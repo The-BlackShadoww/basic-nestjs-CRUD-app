32c9332cdf020efb72947470059b36624f863c48
Ashikur Rahman <ashikur.jebu111@gmail.com>
2026-05-02 19:53:13 +0600

    feat: initialize NestJS users API project with TypeORM and basic CRUD structure

---
+++ src/user/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  create(dto: CreateUserDto) {
+    const user = this.userRepository.create(dto);
+    return this.userRepository.save(user);
+  }
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  async update(id: number, dto: UpdateUserDto) {
+    await this.userRepository.update(id, dto);
+    return this.findOne(id);
+  }
+
+  remove(id: number) {
+    return this.userRepository.delete(id);
+  }
+}
